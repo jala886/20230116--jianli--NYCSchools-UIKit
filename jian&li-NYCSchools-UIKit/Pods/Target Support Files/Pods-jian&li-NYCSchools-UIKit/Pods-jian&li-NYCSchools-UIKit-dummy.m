e7ca49c9f04c20f331fc6203712a55de3ed5b25d
jason li <jason.lee.ticktick@gmail.com>
2023-01-25 22:45:01 -0600

    Add files via upload

---
+++ jian&li-NYCSchools-UIKit/Pods/Target Support Files/Pods-jian&li-NYCSchools-UIKit/Pods-jian&li-NYCSchools-UIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jian_li_NYCSchools_UIKit : NSObject
+@end
+@implementation PodsDummy_Pods_jian_li_NYCSchools_UIKit
+@end
---
+++ jian&li-NYCSchools-UIKit/jian&li-NYCSchools-UIKitTests/jian_li_NYCSchools_UIKitTests.swift
@@ -0,0 +1,47 @@
+//
+//  jian_li_NYCSchools_UIKitTests.swift
+//  jian&li-NYCSchools-UIKitTests
+//
+//  Created by jianli on 1/25/23.
+//
+
+import XCTest
+@testable import jian_li_NYCSchools_UIKit
+
+final class jian_li_NYCSchools_UIKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func test_loadSchoolData() throws {
+        let viewModel = SchoolListViewModel()
+        viewModel.networkManager = MockNetworkManager()
+        let exp = expectation(description: "test")
+        Task {
+            await viewModel.loadSchoolData()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10)
+        XCTAssert(viewModel.schools.count == 2)
+    }
+}
